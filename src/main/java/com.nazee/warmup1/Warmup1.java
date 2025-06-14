4710561d68f2c2716d059bea959f4ed1c5256406
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-14 15:54:30 +0100

    nearHundred

--- src/main/java/com.nazee/warmup1/Warmup1.java
+++ src/main/java/com.nazee/warmup1/Warmup1.java
@@ -43,6 +43,13 @@ public class Warmup1 {
         return false;
     }
 
+    public boolean nearHundred(int n) {
+        if ((n >=90 && n<=110) ||  (n>=190 && n<=210)){
+            return true;
+        }
+        return false;
+    }
+
 
 
 
