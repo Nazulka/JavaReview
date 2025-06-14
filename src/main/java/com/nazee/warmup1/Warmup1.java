91ccb85b004a97ce83e840cb3c7b69f879a465a9
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-14 15:04:08 +0100

    codingBat warmup1

---
+++ src/main/java/com/nazee/warmup1/Warmup1.java
@@ -0,0 +1,20 @@
+package com.nazee.warmup1;
+
+public class Warmup1 {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (! weekday || vacation) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+
+}
