e63ed9566c2208ef56bae67ff727d125322855ef
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-15 19:26:33 +0100

    stringE

--- src/main/java/com.nazee/warmup1/Warmup1.java
+++ src/main/java/com.nazee/warmup1/Warmup1.java
@@ -75,6 +75,15 @@ public class Warmup1 {
         return false;
     }
 
+    public boolean stringE(String str) {
+        int count=0;
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i)=='e') {
+                count++;
+            }
+        }
+        return (count >= 1 && count <= 3);
+    }
 
 
 
