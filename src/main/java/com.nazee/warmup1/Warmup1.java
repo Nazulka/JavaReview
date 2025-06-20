1254a2b1cfc6d13650b64cf79349cad33033acbe
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-20 21:37:16 +0100

    delDel

--- src/main/java/com.nazee/warmup1/Warmup1.java
+++ src/main/java/com.nazee/warmup1/Warmup1.java
@@ -195,8 +195,12 @@ public class Warmup1 {
         return false;
     }
 
-
-
+    public String delDel(String str) {
+        if (str.length()>=4 && str.substring(1, 4).equals("del")) {
+            return str.substring(0, 1) + str.substring(4);
+        }
+        return str;
+    }
 
 
 
