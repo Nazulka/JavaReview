79a2ab6a501ffb41c9359f2db31412d38d7eb45c
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-20 07:56:31 +0100

    in1020

--- src/main/java/com.nazee/warmup1/Warmup1.java
+++ src/main/java/com.nazee/warmup1/Warmup1.java
@@ -176,6 +176,12 @@ public class Warmup1 {
         }
         return false;
     }
+    public boolean in1020(int a, int b) {
+        if ((a>=10 && a<=20) || (b>=10) && (b<=20)) {
+            return true;
+        } return false;
+    }
+
 
 
 
