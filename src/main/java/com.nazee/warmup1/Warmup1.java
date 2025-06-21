d34d4b8f95d5e4d77121b65543ae649647ff1cee
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-21 11:09:07 +0100

    startOz

--- src/main/java/com.nazee/warmup1/Warmup1.java
+++ src/main/java/com.nazee/warmup1/Warmup1.java
@@ -207,7 +207,20 @@ public class Warmup1 {
             return true;
         }
         return false;
+    }
 
+    public String startOz(String str) {
+        String result = "";
+
+        if (str.length() >= 1 && str.charAt(0)=='o') {
+            result = result + str.charAt(0);
+        }
+
+        if (str.length() >= 2 && str.charAt(1)=='z') {
+            result = result + str.charAt(1);
+        }
+
+        return result;
     }
 
 
