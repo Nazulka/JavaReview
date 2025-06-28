06d67f129d44c1a6a3bc45b968aba53648c976bc
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-28 20:04:12 +0100

    stringBits

--- src/main/java/com.nazee/warmup1/Warmup2.java
+++ src/main/java/com.nazee/warmup1/Warmup2.java
@@ -9,5 +9,17 @@ public class Warmup2 {
         if (i+1 >= str.length()) return false; // avoid out-of-bounds error
         return str.substring(i+1, i+2).equals("x");
     }
-
+//str.indexOf("x") searches your string and gives you the
+// index (position) of the first occurrence of the letter "x".
 }
+
+public String stringBits(String str) {
+    String newStr="";
+    for (int i=0; i<str.length(); i+=2) {
+        newStr += str.charAt(i);
+    }
+    return newStr;
+}
+
+
+
