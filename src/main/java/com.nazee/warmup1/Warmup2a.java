98cf9ab0eb92dee8f70b1b02e62417815f2941f9
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-29 15:21:06 +0100

    array123

--- src/main/java/com.nazee/warmup1/Warmup2a.java
+++ src/main/java/com.nazee/warmup1/Warmup2a.java
@@ -83,3 +83,15 @@ public boolean arrayFront9(int[] nums) {
 
     } return false;
 }
+
+public boolean array123(int[] nums) {
+    if (nums.length < 3) {
+        return false;
+    }
+    for (int i=0; i<nums.length-2; i++) {
+        if (nums[i] ==1 && nums[i+1] == 2 && nums[i + 2] == 3) {
+            return true;
+        }
+    }return false;
+}
+
