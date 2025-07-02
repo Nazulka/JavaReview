975ce78819cea5aba18e1823b3f8fb6e3725d361
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-07-02 21:32:57 +0100

    doubleX

--- src/main/java/warmup3/Warmup3.java
+++ src/main/java/warmup3/Warmup3.java
@@ -179,11 +179,23 @@ public class Warmup3 {
                     Math.abs(nums[i+2] - (val-1)) <= 2) {  // the "1" check
                 return true;
             }
-        }
-
-        // If we get here ... none found.
+        } // If we get here ... none found.
         return false;
     }
+
+    boolean doubleX(String str) {
+        int i = str.indexOf("x");
+        if (i == -1) return false; // no "x" at all
+
+        // Is char at i+1 also an "x"?
+        if (i+1 >= str.length()) return false; // check i+1 in bounds?
+        return str.substring(i+1, i+2).equals("x");
+
+        // Another approach -- .startsWith() simplifies the logic
+        // String x = str.substring(i);
+        // return x.startsWith("xx");
+    }
+
 
 
 
