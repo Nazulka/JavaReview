edb224b49e70da00a2496c004685312c940469b6
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-07-07 22:26:28 +0100

    comboString

--- src/main/java/String1.java
+++ src/main/java/String1.java
@@ -14,6 +14,37 @@ public class String1 {
         return tags;
     }
 
+    public String makeOutWord(String out, String word) {
+        String start = out.substring(0, 2);
+        String end = out.substring(2, 4);
+        return start + word + end;
+    }
+
+    public String extraEnd(String str) {
+        String end = str.substring(str.length()-2);
+        return end + end + end;
+
+        // Solution notes: the last two chars begin at index str.length()-2
+        // Here we store the chars in a local variable, which makes the code
+        // longer but simpler.
+    }
+
+    public String firstTwo(String str) {
+        if (str.length()<2) {
+            return str;
+        } else {
+            return str.substring(0, 2);
+        }
+    }
+
+    public String firstHalf(String str) {
+        int first = str.length()/2;
+        return str.substring(0, first);
+    }
+
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length()-1);
+    }
 
 
 }
