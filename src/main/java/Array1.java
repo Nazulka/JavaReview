d343aa1755298f8bd5aa20f8029bbae0f27de68f
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-07-17 11:53:55 +0100

    makeEnds

--- src/main/java/Array1.java
+++ src/main/java/Array1.java
@@ -56,6 +56,13 @@ public class Array1 {
         return middleSum;
     }
 
+    public int[] makeEnds(int[] nums) {
+        int[] firstLast = new int[2];
+        firstLast[0]=nums[0];
+        firstLast[1]=nums[nums.length-1];
+        return firstLast;
+    }
+
 
 
 
