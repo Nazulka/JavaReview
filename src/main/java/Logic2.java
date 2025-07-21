d8777901ce73b881fc874a8e28a13a356fb8c881
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-07-21 20:28:25 +0100

    luckySum

---
+++ src/main/java/Logic2.java
@@ -0,0 +1,31 @@
+public class Logic2 {
+    public boolean makeBricks(int small, int big, int goal) {
+        // not enough bricks
+        if (goal > big*5+small) return false;
+        // not enough small bricks
+        if (goal%5 > small) return false;
+        return true;
+    }
+
+    public int loneSum(int a, int b, int c) {
+        if ((a==b) && (b==c)) return 0;
+        if (a==b) return c;
+        if (b==c) return a;
+        if (a==c) return b;
+        else return a+b+c;
+    }
+
+    public int luckySum(int a, int b, int c) {
+        if (a==13) return 0;
+        if (b==13) return a;
+        if (c==13) return a+b;
+        else return a+b+c;
+    }
+
+
+
+
+
+
+
+}
