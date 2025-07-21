d405019d99df906cf8aaf55f0eaba18ac1780daa
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-07-21 08:27:12 +0100

    fizzString2

--- src/main/java/Logic1.java
+++ src/main/java/Logic1.java
@@ -88,6 +88,89 @@ public class Logic1 {
                 (equalOk && a <= b && b <= c));
     }
 
+    public boolean lastDigit(int a, int b, int c) {
+        return (a%10==b%10) || (a%10==c%10)
+                || (c%10==b%10);
+    }
+
+    public boolean lessBy10(int a, int b, int c) {
+        return (Math.abs(a-b)>=10) || (Math.abs(a-c)>=10) || (Math.abs(b-c)>=10);
+    }
+
+    public int withoutDoubles(int die1, int die2, boolean noDoubles) {
+        if (noDoubles && die1 == die2) {
+            die1 = (die1 == 6) ? 1 : die1 + 1;
+        }
+        return die1 + die2;
+    }
+
+    public int maxMod5(int a, int b) {
+        if (a==b) return 0;
+        if (a%5==b%5) return Math.min(a, b);
+        else return Math.max(a, b);
+    }
+
+    public int redTicket(int a, int b, int c) {
+        if ((a==2 ) && (b==2) && (c==2)) return 10;
+        else if ((a==b) && (b==c)) return 5;
+        else if ((a!=c) && (a!=b)) return 1;
+        else return 0;
+    }
+
+    public int greenTicket(int a, int b, int c) {
+        if ((a!=b) && (b!=c) && (a!=c)) return 0;
+        else if ((a==b) && (b==c)) return 20;
+        else return 10;
+    }
+
+    public int blueTicket(int a, int b, int c) {
+        if ((a+b==10) || (b+c==10) || (a+c==10)) return 10;
+        else if ((a+b==10+b+c) || (a+b==10+a+c)) return 5;
+        else return 0;
+    }
+
+    public boolean shareDigit(int a, int b) {
+        int aTens = a / 10;
+        int aOnes = a % 10;
+        int bTens = b / 10;
+        int bOnes = b % 10;
+        return (aTens == bTens || aTens == bOnes ||
+                aOnes == bTens || aOnes == bOnes);
+    }
+
+    public int sumLimit(int a, int b) {
+        int sum = a + b;
+        if (String.valueOf(sum).length() == String.valueOf(a).length()){
+            return sum;
+        } else {
+            return a;
+        }
+    }
+
+    public int teaParty(int tea, int candy) {
+        if (tea < 5 || candy < 5) return 0; // Bad party
+        if (tea >= 2 * candy || candy >= 2 * tea) return 2; // Great party
+        return 1; // Good party
+    }
+
+    public String fizzString(String str) {
+        if ((str.startsWith("f")) && (str.endsWith("b"))) return "FizzBuzz";
+        if (str.startsWith("f")) return "Fizz";
+        if (str.endsWith("b")) return "Buzz";
+        else return str;
+    }
+
+    public String fizzString2(int n) {
+        if ((n%3==0) && (n%5==0)) return "FizzBuzz!";
+        if ((n%3==0) && (n%5!=0)) return "Fizz!";
+        if (n%5==0) return "Buzz!";
+        else return String.valueOf(n)+"!";
+    }
+
+
+
+
+
 
 
 
