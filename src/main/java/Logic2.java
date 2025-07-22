876dc5be3790318d302b5adb157ea52f2f19efa0
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-07-22 21:01:13 +0100

    evenlySpaced

--- src/main/java/Logic2.java
+++ src/main/java/Logic2.java
@@ -51,6 +51,16 @@ public class Logic2 {
 //        else return b;
     }
 
+    public boolean evenlySpaced(int a, int b, int c) {
+        int max = Math.max(a, Math.max(b, c));
+        int min = Math.min(a, Math.min(b, c));
+        int mid = a + b + c - max - min;
+        // since a + b + c = min + mid + max
+        return (max - mid) == (mid - min);
+    }
+
+
+
 
 
 
