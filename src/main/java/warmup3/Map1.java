1c4d7cb2b1e0efe513a7b6377dc587ca2f68a7ee
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-07-24 08:56:17 +0100

    topping1

--- src/main/java/warmup3/Map1.java
+++ src/main/java/warmup3/Map1.java
@@ -39,6 +39,15 @@ public class Map1 {
         }return map;
     }
 
+    public Map<String, String> topping1(Map<String, String> map) {
+        map.put("bread", "butter");
+        if (map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        } return map;
+
+    }
+
+
 
 
 }
