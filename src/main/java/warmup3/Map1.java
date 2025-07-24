e81c45ce0e8a161c3b9fa15f7ed4bbad6bdf8880
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-07-24 20:27:56 +0100

    mapAB4

--- src/main/java/warmup3/Map1.java
+++ src/main/java/warmup3/Map1.java
@@ -96,6 +96,21 @@ public class Map1 {
         return map;
     }
 
+    public Map<String, String> mapAB4(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            String aVal = map.get("a");
+            String bVal = map.get("b");
+            if (aVal.length() > bVal.length()) {
+                map.put("c", aVal);
+            } else if (aVal.length() < bVal.length()) {
+                map.put("c", bVal);
+            } else {
+                map.put("a", "");
+                map.put("b", "");
+            }
+        } return map;
+    }
+
 
 
 
