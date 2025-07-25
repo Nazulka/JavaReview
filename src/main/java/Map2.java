36b03385b1c0abafb86e39e0763afb07778a243c
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-07-25 21:40:17 +0100

    word0

--- src/main/java/Map2.java
+++ src/main/java/Map2.java
@@ -1,2 +1,11 @@
 public class Map2 {
+    public Map<String, Integer> word0(String[] strings) {
+        Map<String, Integer> map = new HashMap();
+        for (String s:strings) {
+            map.put(s, 0);
+        }
+        return map;
+    }
+
+
 }
