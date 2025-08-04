d7355388c748b83f4ce73a886c9a27e66ee62adf
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-08-04 19:46:35 +0100

    construct2DArray2022

--- src/main/java/LeetcodeProblems/LeetCodew11_14.java
+++ src/main/java/LeetcodeProblems/LeetCodew11_14.java
@@ -189,6 +189,21 @@ public class LeetCodew11_14 {
         return j;
     }
 
+    public int[][] construct2DArray2022(int[] original, int m, int n) {
+        if (original.length != m*n){
+            return new int[0][0];
+        }
+        int [][] arr = new int [m][n];
+        int current_index = 0;
+        for (int i=0; i<m; i++) { // top to bottom
+            for (int j=0; j<n; j++) { //left ot right over the columns
+                arr [i][j] = original[current_index];
+                current_index++;
+            }
+        }
+        return arr;
+    }
+
 
 
 
