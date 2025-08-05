d13d1af82af60242bb3da078d0451876b5ab9571
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-08-05 20:32:12 +0100

    merge88

--- src/main/java/LeetcodeProblems/LeetCodew11_14.java
+++ src/main/java/LeetcodeProblems/LeetCodew11_14.java
@@ -304,6 +304,30 @@ public class LeetCodew11_14 {
         return insertPosition;
     }
 
+    public void merge88(int[] nums1, int m, int[] nums2, int n) {
+        int tail1 = m-1; // pointer at the largest value in nums1
+        int tail2 = n-1; // pointer at largest value in nums2
+        int insertPosition = m+n-1;
+        // the very end of nums1, where we will start putting
+        // the merging arrays nums1 and nums2
+
+        // keep merging until we run out of elements in nums2
+        while (tail2 >=0) {
+            if (tail1>=0 && nums1[tail1] > nums2[tail2]) {
+                // if nums1 has the next element and if it is greater then then
+                // next element in nums2, then put the next element from nums1 in the
+                // insertPosition
+                // and decrement both insertPOsition and tail1
+                nums1[insertPosition--] = nums1[tail1--];
+            } else {
+                // otherwise, put the next element from nums2
+                // in the insertPOsition and decrement both
+                // insertPOsition and tail2
+                nums1[insertPosition--] = nums2[tail2--];
+            }
+        }
+    }
+
 
 
 
