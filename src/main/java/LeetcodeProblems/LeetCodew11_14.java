0b883eee576210f6b22e2e7a7cfbe5c63e083db7
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-08-06 21:41:30 +0100

    plusOne66

--- src/main/java/LeetcodeProblems/LeetCodew11_14.java
+++ src/main/java/LeetcodeProblems/LeetCodew11_14.java
@@ -361,6 +361,19 @@ public class LeetCodew11_14 {
         return result;
     }
 
+    public int[] plusOne66(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newArr = new int[digits.length + 1];
+        newArr[0] = 1;
+        return newArr;
+    }
+
 
 
 
