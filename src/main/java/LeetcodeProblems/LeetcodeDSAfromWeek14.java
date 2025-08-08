3ce761f9ae5072d3e493c728cfeecae2b335125e
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-08-08 21:32:06 +0100

    searchInsert

--- src/main/java/LeetcodeProblems/LeetcodeDSAfromWeek14.java
+++ src/main/java/LeetcodeProblems/LeetcodeDSAfromWeek14.java
@@ -1,6 +1,12 @@
 package LeetcodeProblems;
 
 public class LeetcodeDSAfromWeek14 {
-
+    public int searchInsert(int[] nums, int target) {
+        for (int i=0; i<nums.length; i++) {
+            if ((nums[i]==target) || (nums[i]>target))  {
+                return i;
+            }
+        } return nums.length;
+    }
 
 }
