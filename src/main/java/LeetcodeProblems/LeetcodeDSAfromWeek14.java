a13db154a7d62eebea4193149a2e6dc2f557ec78
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-08-09 21:55:38 +0100

    containsNearbyDuplicateA

--- src/main/java/LeetcodeProblems/LeetcodeDSAfromWeek14.java
+++ src/main/java/LeetcodeProblems/LeetcodeDSAfromWeek14.java
@@ -20,6 +20,17 @@ public class LeetcodeDSAfromWeek14 {
         }return false;
     }
 
+    public boolean containsNearbyDuplicateA(int[] nums, int k) {
+        for (int i=0; i<nums.length; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                if ((nums[i]==nums[j]) && Math.abs(i-j)<=k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 
 
 
