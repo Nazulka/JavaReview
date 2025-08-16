5e0611dfea8e2d4fe7447bc99be4e7058ffa3722
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-08-16 21:30:25 +0100

    spiralOrder54

--- src/main/java/LeetcodeProblems/LeetCodew11_14.java
+++ src/main/java/LeetcodeProblems/LeetCodew11_14.java
@@ -374,6 +374,32 @@ public class LeetCodew11_14 {
         return newArr;
     }
 
+    public List<Integer> spiralOrder54(int[][] matrix) {
+        List<Integer> res= new ArrayList<Integer>();
+        if (matrix.length==0 || matrix[0].length==0) {
+            return res;
+        }
+        int top=0;
+        int bottom=matrix.length-1;
+        int left = 0;
+        int right = matrix[0].length-1;
+        while(true) {
+            // 1. iterate over top row, left to right
+            for (int i=left; i<=right; i++) res.add(matrix[top][i]);
+            if (++top>bottom) break;
+            // 2. iterate over right column, top to bottom
+            for (int i=top; i<=bottom; i++) res.add(matrix[i][right]);
+            if (left>--right) break;
+            // 3. Iterate over bottom row, right to left
+            for (int i=right; i>=left; i--) res.add(matrix[bottom][i]);
+            if (top>--bottom) break;
+            // 4. Iterate over left column, bottom to top
+            for (int i=bottom; i>=top; i--) res.add(matrix[i][left]);
+            if (++left > right) break;
+        }
+        return res;
+    }
+
 
 
 
