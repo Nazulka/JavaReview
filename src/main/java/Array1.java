e333bf852a8e153989c0e3bda93d29e69a352c34
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-09-07 18:35:16 +0100

    only14

--- src/main/java/Array1.java
+++ src/main/java/Array1.java
@@ -231,6 +231,15 @@ public class Array1 {
                 (temp >= 60 && temp <= 100 && isSummer);
     }
 
+    public boolean only14(int[] nums) {
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]!=1 && nums[i]!=4) {
+                return false;
+            }
+        } return true;
+    }
+
+
 
 
 
