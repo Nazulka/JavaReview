2a9c487bd1c62bea2e6a43f1aee8fc8b22576900
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-09-07 18:42:44 +0100

    no14

--- src/main/java/Array1.java
+++ src/main/java/Array1.java
@@ -239,6 +239,19 @@ public class Array1 {
         } return true;
     }
 
+    public boolean no14(int[] nums) {
+        boolean has1 = false;
+        boolean has4 = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) has1 = true;
+            if (nums[i] == 4) has4 = true;
+        }
+
+        return !(has1 && has4); // true if NOT both
+    }
+
+
 
 
 
