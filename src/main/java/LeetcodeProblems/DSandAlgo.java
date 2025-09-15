6787420a1f27d9e4db8d8e89a4ce2389f94d9ce7
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-09-15 21:30:20 +0100

    findDisappearedNumbers

--- src/main/java/LeetcodeProblems/DSandAlgo.java
+++ src/main/java/LeetcodeProblems/DSandAlgo.java
@@ -1,4 +1,38 @@
 package LeetcodeProblems;
 
-public class ShabdanLectures {
+public class DSandAlgo {
+    // print yes if you can get 1 from N, No if not
+
+    // n! factorial = 1*2*3*...*n
+    // 3!=1*2*3
+    // --> 2 options (+3 and +5) = O(2^n)
+    // binary tree
+
+
+    // 448. Find All Numbers Disappeared in an Array
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        List<Integer> result = new ArrayList<>();
+
+        // check each number from 1..n
+        for (int i = 1; i <= n; i++) {
+            boolean found = false;
+
+            // search for i in nums
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == i) {
+                    found = true;
+                    break; // no need to keep checking
+                }
+            }
+
+            // if not found, then i is missing
+            if (!found) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+
 }
